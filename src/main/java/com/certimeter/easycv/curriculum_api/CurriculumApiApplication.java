6361e5fde9e236c5e4da7f6ac6c3f2b7de39c1b3
giuseppe.falcone <giuseppe.falcone@lutech.it>
2025-10-21 14:55:27 +0200

    init spring boot application

---
+++ src/main/java/com/certimeter/easycv/curriculum_api/CurriculumApiApplication.java
@@ -0,0 +1,13 @@
+package com.certimeter.easycv.curriculum_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/certimeter/easycv/curriculum_api/CurriculumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certimeter.easycv.curriculum_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
